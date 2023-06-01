118236a1a53c71e0b0b2114c8c396d5ea1771c3f
Aytaç Aydemir <aytacaydemr@gmail.com>
2023-06-02 00:04:49 +0300

    another dto converter created for ingredient usage response

--- src/main/java/com/aytac/recipeshare/dto/converter/IngredientResponseConverter.java
+++ src/main/java/com/aytac/recipeshare/dto/converter/IngredientResponseConverter.java
@@ -1,13 +1,23 @@
 package com.aytac.recipeshare.dto.converter;
 
 import com.aytac.recipeshare.dto.response.IngredientResponse;
+import com.aytac.recipeshare.dto.response.IngredientUsageResponse;
 import com.aytac.recipeshare.model.Ingredient;
 import org.springframework.stereotype.Component;
 
 @Component
 public class IngredientResponseConverter {
-    public IngredientResponse convert(Ingredient ingredient) {
-        
-        return new IngredientResponse(ingredient.getId(), ingredient.getName());
+    public IngredientResponse convertToIngredientResponse(Ingredient ingredient) {
+
+        return new IngredientResponse(
+                ingredient.getId(),
+                ingredient.getName());
+    }
+
+    public IngredientUsageResponse convertToIngredientUsageResponse(Ingredient ingredient) {
+        return new IngredientUsageResponse(
+                ingredient.getId(),
+                ingredient.getName(),
+                ingredient.getRecipes().size());
     }
 }
