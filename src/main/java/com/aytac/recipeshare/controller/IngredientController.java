2c0f842028e49d07a7e0b095bc66e8aa96c37022
Aytaç Aydemir <aytacaydemr@gmail.com>
2023-06-02 00:02:56 +0300

    added /top-five endpoint -controller and service method-

--- src/main/java/com/aytac/recipeshare/controller/IngredientController.java
+++ src/main/java/com/aytac/recipeshare/controller/IngredientController.java
@@ -2,6 +2,7 @@ package com.aytac.recipeshare.controller;
 
 import com.aytac.recipeshare.dto.request.IngredientCreateRequest;
 import com.aytac.recipeshare.dto.response.IngredientResponse;
+import com.aytac.recipeshare.dto.response.IngredientUsageResponse;
 import com.aytac.recipeshare.service.IngredientService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,9 +24,16 @@ public class IngredientController {
         return ResponseEntity.ok(ingredientService.getIngredients());
     }
 
+    @GetMapping("/top-five")
+    public ResponseEntity<List<IngredientUsageResponse>> getTopFiveIngredients(){
+        return ResponseEntity.ok(ingredientService.getTopFiveIngredients());
+    }
+
     @PostMapping
     public ResponseEntity<IngredientResponse> createIngredient(@RequestBody IngredientCreateRequest request){
         return ResponseEntity.ok(ingredientService.createIngredient(request));
     }
+
+
 
 }
--- src/main/java/com/aytac/recipeshare/service/IngredientService.java
+++ src/main/java/com/aytac/recipeshare/service/IngredientService.java
@@ -3,10 +3,13 @@ package com.aytac.recipeshare.service;
 import com.aytac.recipeshare.dto.converter.IngredientResponseConverter;
 import com.aytac.recipeshare.dto.request.IngredientCreateRequest;
 import com.aytac.recipeshare.dto.response.IngredientResponse;
+import com.aytac.recipeshare.dto.response.IngredientUsageResponse;
 import com.aytac.recipeshare.model.Ingredient;
 import com.aytac.recipeshare.repository.IngredientRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,7 +28,7 @@ public class IngredientService {
         List<Ingredient> ingredients = ingredientRepository.findAll();
 
         return ingredients.stream()
-                .map(converter::convert)
+                .map(converter::convertToIngredientResponse)
                 .collect(Collectors.toList());
     }
 
@@ -34,10 +37,23 @@ public class IngredientService {
         Ingredient ingredient = new Ingredient();
         ingredient.setName(request.name());
 
-        return converter.convert(ingredientRepository.save(ingredient));
+        return converter.convertToIngredientResponse(ingredientRepository.save(ingredient));
     }
 
     public Ingredient getIngredientById(Long id){
         return ingredientRepository.findById(id).orElse(null);
+    }
+
+    public List<IngredientUsageResponse> getTopFiveIngredients() {
+        List<Ingredient> ingredients = ingredientRepository.findAll();
+        Collections.sort(ingredients, Collections.reverseOrder(new IngredientComparator()));
+        List<Ingredient> topFiveIngredients = ingredients.subList(0, Math.min(ingredients.size(),5));
+        return topFiveIngredients.stream().map(converter::convertToIngredientUsageResponse).collect(Collectors.toList());
+    }
+}
+class IngredientComparator implements Comparator<Ingredient> {
+    @Override
+    public int compare(Ingredient o1, Ingredient o2) {
+        return Integer.compare(o1.getRecipes().size(), o2.getRecipes().size());
     }
 }
