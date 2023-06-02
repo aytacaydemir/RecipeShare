5198057b0f6270f1c075c1d5fc3ec42b8503a9fc
Aytaç Aydemir <aytacaydemr@gmail.com>
2023-06-02 16:27:33 +0300

    findByIngredientsId method created with Spring Data JPA's method naming convention for finding recipe list which includes specific ingredient id

--- src/main/java/com/aytac/recipeshare/repository/RecipeRepository.java
+++ src/main/java/com/aytac/recipeshare/repository/RecipeRepository.java
@@ -3,5 +3,8 @@ package com.aytac.recipeshare.repository;
 import com.aytac.recipeshare.model.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+    List<Recipe> findByIngredientsId(Long ingredientId);
 }
